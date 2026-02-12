cf4b9df97b794cd0d1e750cc44a0c5f43127fdd8
Hong Hoa Nguyen <hong-hoa.nguyen@etu.umontpellier.fr>
2026-02-12 15:30:53 +0100

    Add piece field to BoxData

--- src/main/java/com/vivianhonghoa/chess/model/BoxData.java
+++ src/main/java/com/vivianhonghoa/chess/model/BoxData.java
@@ -4,11 +4,13 @@ public class BoxData {
     private final int row;
     private final int col;
     private BoxState state;
+    private Piece piece;
 
     public BoxData(int row, int col) {
         this.row = row;
         this.col = col;
         this.state = BoxState.DEFAULT;
+        this.piece = null;
     }
 
 
@@ -16,6 +18,10 @@ public class BoxData {
     public int getCol() { return col; }
     public BoxState getState() { return state; }
     public void setState(BoxState state) { this.state = state; }
+
+    public Piece getPiece() { return piece; }
+    public void setPiece(Piece piece) { this.piece = piece; }
+    public boolean isEmpty() { return piece == null; }
 
     public boolean checkPosition(int row, int col) {
         return this.row == row && this.col == col;
