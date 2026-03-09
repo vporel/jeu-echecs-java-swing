c2a8389e7b0c6bc5237e8e12dd7f77ae5786c86e
Hong Hoa Nguyen <hong-hoa.nguyen@etu.umontpellier.fr>
2026-03-09 10:20:59 +0100

    add Piece base class + position tracking + sliding movement helper

--- src/main/java/com/vivianhonghoa/chess/model/pieces/Piece.java
+++ src/main/java/com/vivianhonghoa/chess/model/pieces/Piece.java
@@ -2,10 +2,14 @@ package com.vivianhonghoa.chess.model.pieces;
 
 import com.vivianhonghoa.chess.model.Board;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Piece {
     private final Color color;
+    protected Board board;
+    protected int row;
+    protected int col;
 
     protected Piece(Color color) {
         this.color = color;
@@ -15,7 +19,45 @@ public abstract class Piece {
         return color;
     }
 
+    public void setPosition(Board board, int row, int col) {
+        this.board = board;
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
     public abstract List<Board.Case> getAccessibleCasses();
+
+    /**
+     * Helper for sliding pieces (rook, bishop, queen).
+     * Walks in a direction until leaving the board or hitting a piece.
+     */
+    protected List<Board.Case> getCasesInDirection(int dRow, int dCol) {
+        List<Board.Case> cases = new ArrayList<>();
+        int r = row + dRow;
+        int c = col + dCol;
+        while (Board.Case.isValid(r, c)) {
+            Piece target = board.getPiece(r, c);
+            if (target == null) {
+                cases.add(new Board.Case(r, c));
+            } else {
+                if (target.getColor() != this.color) {
+                    cases.add(new Board.Case(r, c));
+                }
+                break;
+            }
+            r += dRow;
+            c += dCol;
+        }
+        return cases;
+    }
 
     public enum Color {
         BLANC,
